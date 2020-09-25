7fadf93f886f4190386342cddb47bf2b04f6b7d7
Nazmul Huda <mhnazmul48@gmail.com>
2020-09-25 16:03:23 +0600

    Hot Onion Ui Setup Done

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1 className='jumbotron text-center font-width-bold'>Programmer Nazmul</h1>
+        </div>
+    );
+};
+
+export default Main;
